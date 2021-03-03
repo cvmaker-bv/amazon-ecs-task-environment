8cd91192f5bae6f72c647689fdca09870aa98e09
Yuri Drabik <yurist38@mail.ru>
2021-03-03 13:36:41 +0100

    Refactored

--- src/index.ts
+++ src/index.ts
@@ -7,50 +7,45 @@ import { Env } from './types';
 
 async function run(): Promise<void> {
   try {
-    debug('START ADDING VARIABLES');
     const taskPathValue: string = getInput('task-definition');
     const taskDefPath = isAbsolute(taskPathValue)
       ? taskPathValue
       : join(process.env.GITHUB_WORKSPACE || __dirname, taskPathValue);
-    debug(taskDefPath);
     const taskContent = readFileSync(taskDefPath).toString();
-    debug(taskContent);
-    debug('END');
-    // const taskDef: ECS.TaskDefinition = JSON.parse(taskContent);
-    // debug(JSON.stringify(taskDef));
+    const taskDef: ECS.TaskDefinition = JSON.parse(taskContent);
 
-    // if (!taskDef.containerDefinitions) {
-    //   throw new Error('The task does not have any containers defined...');
-    // }
+    if (!taskDef.containerDefinitions) {
+      throw new Error('The task does not have any containers defined...');
+    }
 
-    // const containerName: string = getInput('container-name');
-    // const containerIndex = taskDef.containerDefinitions
-    //   .findIndex(({ name }) => name === containerName)
+    const containerName: string = getInput('container-name');
+    const containerIndex = taskDef.containerDefinitions
+      .findIndex(({ name }) => name === containerName)
 
-    // if (containerIndex === undefined) {
-    //   throw new Error(`Container ${containerName} is not found in the task definition...`);
-    // }
+    if (containerIndex === undefined) {
+      throw new Error(`Container ${containerName} is not found in the task definition...`);
+    }
 
-    // const envVars: Env = JSON.parse(getInput('env-variables'));
+    const envVars: Env = JSON.parse(getInput('env-variables'));
 
-    // taskDef.containerDefinitions[containerIndex].environment = [
-    //   ...(taskDef.containerDefinitions[containerIndex].environment || []),
-    //   ...Object.keys(envVars).map((name) => ({ name, value: envVars[name] }))
-    // ];
+    taskDef.containerDefinitions[containerIndex].environment = [
+      ...(taskDef.containerDefinitions[containerIndex].environment || []),
+      ...Object.keys(envVars).map((name) => ({ name, value: envVars[name] }))
+    ];
 
-    // debug(taskDef.toString());
+    debug(taskDef.toString());
 
-    // const updatedTaskDefFile = fileSync({
-    //   tmpdir: process.env.RUNNER_TEMP,
-    //   prefix: 'task-definition-',
-    //   postfix: '.json',
-    //   keep: true,
-    //   discardDescriptor: true
-    // });
+    const updatedTaskDefFile = fileSync({
+      tmpdir: process.env.RUNNER_TEMP,
+      prefix: 'task-definition-',
+      postfix: '.json',
+      keep: true,
+      discardDescriptor: true
+    });
 
-    // const newTaskDefContent = JSON.stringify(taskDef, null, 2);
-    // writeFileSync(updatedTaskDefFile.name, newTaskDefContent);
-    // setOutput('task-definition', updatedTaskDefFile.name);
+    const newTaskDefContent = JSON.stringify(taskDef, null, 2);
+    writeFileSync(updatedTaskDefFile.name, newTaskDefContent);
+    setOutput('task-definition', updatedTaskDefFile.name);
   } catch (error) {
     setFailed(error.message);
     debug(error.stack);
