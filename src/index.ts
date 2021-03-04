f8038a6ef26b673145d06deeb0f34a5a2a5684ba
Yuri Drabik <yurist38@mail.ru>
2021-03-04 11:28:42 +0100

    V1 (#1)
    * Init v1
    
    * Downgrade Node version to 12
    
    * test
    
    * Renamed input parameter
    
    * test
    
    * Added husky
    
    * Refactored
    
    * Test output
    
    * Removed log message
    
    * Added readme
    
    * Updated readme
    
    * Updated gitignore

---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import { debug, getInput, setOutput, setFailed } from '@actions/core';
+import { readFileSync, writeFileSync } from 'fs';
+import { isAbsolute, join } from 'path';
+import { fileSync } from 'tmp';
+import { ECS } from 'aws-sdk';
+import { Env } from './types';
+
+export function run(): void {
+  try {
+    const taskPathValue: string = getInput('task-definition');
+    const taskDefPath = isAbsolute(taskPathValue)
+      ? taskPathValue
+      : join(process.env.GITHUB_WORKSPACE || __dirname, taskPathValue);
+    const taskContent = readFileSync(taskDefPath).toString();
+    const taskDef: ECS.TaskDefinition = JSON.parse(taskContent);
+
+    if (!taskDef.containerDefinitions) {
+      throw new Error('The task does not have any containers defined...');
+    }
+
+    const containerName: string = getInput('container-name');
+    const containerIndex = taskDef.containerDefinitions
+      .findIndex(({ name }) => name === containerName)
+
+    if (containerIndex === undefined) {
+      throw new Error(`Container ${containerName} is not found in the task definition...`);
+    }
+
+    const envVars: Env = JSON.parse(getInput('env-variables'));
+
+    taskDef.containerDefinitions[containerIndex].environment = [
+      ...(taskDef.containerDefinitions[containerIndex].environment || []),
+      ...Object.keys(envVars).map((name) => ({ name, value: envVars[name] }))
+    ];
+
+    const updatedTaskDefFile = fileSync({
+      tmpdir: process.env.RUNNER_TEMP,
+      prefix: 'task-definition-',
+      postfix: '.json',
+      keep: true,
+      discardDescriptor: true
+    });
+
+    const newTaskDefContent = JSON.stringify(taskDef, null, 2);
+    writeFileSync(updatedTaskDefFile.name, newTaskDefContent);
+    setOutput('task-definition', updatedTaskDefFile.name);
+  } catch (error) {
+    setFailed(error.message);
+    debug(error.stack);
+  }
+}
+
+if (require.main === module) {
+  run();
+}
---
+++ src/types.ts
@@ -0,0 +1,3 @@
+export type Env = {
+  [name: string]: string;
+}
